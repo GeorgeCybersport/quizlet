0b043f23e9a34b14358ffbf9e485b7b41900cab3
Alaya95 <rozetta.95@mail.ru>
2022-09-29 00:09:20 +0300

    created config eslintrc.json. edited files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <div>123</div>
-  </StrictMode>
+  </StrictMode>,
 );
