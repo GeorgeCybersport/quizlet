e064ed130c8809e23d26cc1378d58fa54f898176
Alaya95 <rozetta.95@mail.ru>
2022-10-29 16:28:47 +0300

    edited

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <div className={styles.div}>
-      <FlashCards text='fdasfs'/>
+      <FlashCards />
     </div>
   </StrictMode>
 );
--- src/shared/ui/FlashCards/FlashCards.tsx
+++ src/shared/ui/FlashCards/FlashCards.tsx
@@ -1,36 +1,26 @@
 import { classNames } from '../../lib/classNames';
-import React, { FC, useRef } from 'react';
+import React, { FC } from 'react';
 import cls from './styles.module.scss';
-import { Button } from '../Button/Button';
-interface Props {
-  text?: string
-};
 
-export const FlashCards: FC <Props> = ({ text }) => {
-  console.log(text);
-  const starRef = useRef();
+interface Props {};
 
+export const FlashCards: FC <Props> = () => {
   return (
     <div className={cls.card}>
       <div className={cls.card__header}>
-        <div>Термин</div>
-        <div className={cls.card__progress}>3 / 37</div>
-        <div className={cls.card__star}>
-
-          <button type='button' >
+        <p>Термин</p>
+        <p className={cls.card__progress}>3 / 37</p>
+        <p className={cls.card__star}>
+          <button type='button'>
             <span
               className={ classNames('material-symbols-outlined', {}, [cls.icons])}
-            >
-              star
-            </span>
+            >star</span>
           </button>
-        </div>
+        </p>
       </div>
-
       <div className={cls.card__content}>
         <span className={cls.card__word}>hire</span>
       </div>
-
       <div className={cls.card__footer}>
         <div className={classNames(cls.card__arrow, {}, [cls.card__arrowLeft])}>
           <button type='button'>
@@ -39,7 +29,6 @@ export const FlashCards: FC <Props> = ({ text }) => {
             </span>
           </button>
         </div>
-
         <div className={classNames(cls.card__arrow, {}, [cls.card__arrowRight])}>
           <button type='button'>
             <span className={ classNames('material-symbols-outlined', {}, [cls.icons])}>
